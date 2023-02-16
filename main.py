99c61b1d10704600dd5e3da61866855408db6a8d
Vanidor <111648145+Vanidor@users.noreply.github.com>
2023-02-16 15:32:13 +0100

    Replace getopt with argparse

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 ''' Logging library for logging '''
 import logging as log
-import sys
-import getopt
+import argparse
 import bot
 
 LOGFORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
-LOGLEVEL = log.DEBUG
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-t',
+    '--token',
+    required=True,
+    help='The twitch OAuth token for the bot. https://twitchtokengenerator.com'
+)
+parser.add_argument(
+    '-p',
+    '--prefix',
+    required=True,
+    help='The prefix of the bot.'
+)
+parser.add_argument(
+    '--loglevel',
+    default=log.INFO,
+    choices=log._nameToLevel.keys()  # pylint: disable=W0212
+)
+
+args = parser.parse_args()
+
+TOKEN = args.token
+PREFIX = args.prefix
+LOGLEVEL = args.loglevel
+
+print(f"Token: {TOKEN}\r\nPREFIX: {PREFIX}\r\nLOGLEVEL: {LOGLEVEL}")
 
 log.basicConfig(
     filename='py.log',
@@ -18,37 +42,15 @@ console = log.StreamHandler()
 console.setLevel(LOGLEVEL)
 log.getLogger('').addHandler(console)
 
-opts, args = getopt.getopt(sys.argv[1:], "t:p:", ["token=", "prefix="])
-
-TOKEN = ""
-PREFIX = ""
-
-for opt, arg in opts:
-    if opt == '--token':
-        log.info("Token: %s", arg)
-        TOKEN = arg
-    elif opt == '--prefix':
-        log.info("Prefix: %s", arg)
-        PREFIX = arg
-
-if TOKEN == "":
-    log.error("Token can't be empty!")
-    raise AttributeError
-
-if PREFIX == "":
-    log.error("Prefix can't be empty!")
-    raise AttributeError
-
-
 channels = [
     "vanidor",
     "vanib0t"
 ]
-AdminUsers=[
+AdminUsers = [
     "vanidor"
 ]
 log.info(
-    "Creating bot with token '%s' and prefix '%s' for channel channels '%s'", 
+    "Creating bot with token '%s' and prefix '%s' for channel channels '%s'",
     TOKEN, PREFIX, channels)
 
 bot = bot.Bot(
@@ -63,6 +65,6 @@ log.info("Created bot, starting mainloop")
 
 try:
     bot.run()
-except (Exception) as e: # pylint: disable=broad-except
+except (Exception) as e:  # pylint: disable=broad-except
     log.error("Error in bot mainloop. Type: %s", type(e))
     log.error("Stacktrace: %s", e)
