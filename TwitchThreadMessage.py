d6680a7f970f71e4751306cce21a9919fe6aeb5a
Vanidor <111648145+Vanidor@users.noreply.github.com>
2023-04-02 15:31:56 +0200

    Remove not needed thread_id and message_id

--- TwitchThreadMessage.py
+++ TwitchThreadMessage.py
@@ -1,5 +1,4 @@
 class TwitchThreadMessage():
-    def __init__(self, message_id: str, message_author: str, message_content: str):
-        self.message_id = message_id
+    def __init__(self, message_author: str, message_content: str):
         self.message_author = message_author
         self.message_content = message_content
