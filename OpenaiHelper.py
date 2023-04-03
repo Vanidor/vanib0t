34badcc74390e90f07bc3345cc31acefc5b59f74
Vanidor <111648145+Vanidor@users.noreply.github.com>
2023-04-03 22:42:01 +0200

    Set max_tokens to 100

--- OpenaiHelper.py
+++ OpenaiHelper.py
@@ -30,7 +30,7 @@ class OpenaiHelper:
                 temperature=1,
                 top_p=1,
                 n=1,
-                max_tokens=200,
+                max_tokens=100,
                 presence_penalty=0,
                 frequency_penalty=0,
                 user=username
