731cfe7d2e45aa0fb32be9de0d8e047b1c60d44b
Vanidor <111648145+Vanidor@users.noreply.github.com>
2023-04-28 17:08:20 +0200

    Add chatgpt_prompt var

--- database/ChannelSettings.py
+++ database/ChannelSettings.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from Base import Base
 
+
 class ChannelSettings(Base):
     ''' Class representing channel settings '''
     __tablename__ = "channel_settings"
 
     channel_id: Mapped[int] = mapped_column(primary_key=True)
     display_name: Mapped[str] = mapped_column(String(length=30))
+    chatgpt_prompt: Mapped[str] = mapped_column(String())
     feature_chatgpt: Mapped[bool]
     feature_fishh: Mapped[bool]
     feature_streamschedule: Mapped[bool]
